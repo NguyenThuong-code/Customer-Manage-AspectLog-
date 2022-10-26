9ceb679e628d38c3dbf14b1bea54cda3d5d5e415
NguyenThuong-code <you@example.com>
2022-10-26 11:31:15 +0700

    Aspect Log

---
+++ src/main/java/com/thuong/repository/ProvinceRepository.java
@@ -0,0 +1,7 @@
+package com.thuong.repository;
+
+import com.thuong.model.Province;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProvinceRepository extends CrudRepository<Province,Long> {
+}
